148f4108ca9d3eb28b225396968554dd993c1083
wandesky <bwandesky@gmail.com>
2018-10-12 20:35:00 +0300

    done chapter two

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get("User-Agent")
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
